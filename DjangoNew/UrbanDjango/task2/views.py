9d242609b654fc905845ab384ced189c0ad0a2d5
gnik112 <gnik112@yandex.ru>
2024-12-16 17:41:50 +0300

    first mess

---
+++ DjangoNew/UrbanDjango/task2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+def index_func(request):
+    return render(request, 'second_task/func_template.html')
+
+class index_class(TemplateView):
+    template_name = 'second_task/class_template.html'
